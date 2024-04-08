25aef41bf6966cb9902815d9bf09e603b7f5e338
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-08 18:54:48 +0300

    feat: update Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,19 +2,22 @@ import { Suspense } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 
 import { GlobalStyle } from "@/styles/GlobalStyle";
+import * as SC from "./Layout.styled";
 
 export const Layout = () => {
   return (
-    <>
+    <SC.Container>
       <GlobalStyle />
 
       <NavLink to="/">Home</NavLink>
       <NavLink to="/catalog">Catalog</NavLink>
       <NavLink to="/favorites"> Favorites</NavLink>
 
-      <Suspense>
-        <Outlet />
-      </Suspense>
-    </>
+      <main>
+        <Suspense>
+          <Outlet />
+        </Suspense>
+      </main>
+    </SC.Container>
   );
 };
---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  padding-right: ${(p) => p.theme.spacing(16)};
+  padding-left: ${(p) => p.theme.spacing(16)};
+`;
