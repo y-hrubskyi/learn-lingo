02e8315c432d5fa7fd7caddc8afeb83902f0f140
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-08 16:54:15 +0300

    feat: init route-library and configure routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { Layout } from "@/components/Layout/Layout";
+import HomePage from "@/pages/HomePage/HomePage";
+import CatalogPage from "@/pages/CatalogPage/CatalogPage";
+import FavoritesPage from "@/pages/FavoritesPage/FavoritesPage";
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
 };
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 export const Layout = () => {
-  return <div>Layout</div>;
+  return <Outlet />;
 };
