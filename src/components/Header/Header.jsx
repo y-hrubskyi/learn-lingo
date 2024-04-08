5b6cca95ae21e000212f4358eda72adb397e4bd8
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-08 19:03:41 +0300

    feat: create Logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
 import * as SC from "./Header.styled";
 
 export const Header = () => {
-  return <SC.Header />;
+  return (
+    <SC.Header>
+      <SC.Logo to="/">
+        <SC.LogoIcon />
+        LearnLingo
+      </SC.Logo>
+    </SC.Header>
+  );
 };
