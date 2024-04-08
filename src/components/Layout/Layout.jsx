229cdcdbcde91a20c208955479dfe7394386dd4c
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-08 17:01:31 +0300

    feat: add basic navigation

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,16 @@
 import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
-    <Suspense>
-      <Outlet />
-    </Suspense>
+    <>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/catalog">Catalog</NavLink>
+      <NavLink to="/favorites"> Favorites</NavLink>
+
+      <Suspense>
+        <Outlet />
+      </Suspense>
+    </>
   );
 };
