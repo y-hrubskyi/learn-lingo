5d1c1687ed70101c6045762c45cd0dc0d8627ab9
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-12 23:40:12 +0300

    feat: create reusable FormField component

---
+++ src/components/common/FormField/FormField.jsx
@@ -0,0 +1,10 @@
+import * as SC from "./FormField.styled";
+
+export const FormField = ({ placeholder, type, name }) => {
+  return (
+    <SC.FieldWrapper>
+      <SC.Field type={type} name={name} placeholder={placeholder} />
+      <SC.ErrorMessage name={name} component="span" />
+    </SC.FieldWrapper>
+  );
+};
---
+++ src/components/common/FormField/FormField.styled.js
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import { Field as FormikField, ErrorMessage as FormikError } from "formik";
+
+export const FieldWrapper = styled.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  gap: ${(p) => p.theme.spacing(1)};
+`;
+
+export const Field = styled(FormikField)`
+  padding: ${(p) => p.theme.spacing(4)} ${(p) => p.theme.spacing(4.5)};
+
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 1.375;
+  color: ${(p) => p.theme.colors.primary()};
+
+  background: transparent;
+  border: 1px solid ${(p) => p.theme.colors.primary(0.1)};
+  border-radius: ${(p) => p.theme.radii.btn};
+
+  &::placeholder {
+    color: currentColor;
+  }
+`;
+
+export const ErrorMessage = styled(FormikError)`
+  color: ${(p) => p.theme.colors.red};
+  font-size: 14px;
+`;
