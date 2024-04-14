2be7c9ce162077acbe3da88442dacf7da787f387
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-14 23:31:46 +0300

    feat: upd AuthNav component (add auth-modals logic)

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useState } from "react";
+
+import { RegisterModal } from "@/components/RegisterModal/RegisterModal";
+import { LoginModal } from "@/components/LoginModal/LoginModal";
 import * as SC from "./AuthNav.styled";
 
 export const AuthNav = () => {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [isRegisterOpen, setIsRegisterOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.style.overflow =
+      isLoginOpen || isRegisterOpen ? "hidden" : "unset";
+  }, [isLoginOpen, isRegisterOpen]);
+
+  const toggleLoginModal = () => {
+    setIsLoginOpen((prevState) => !prevState);
+  };
+
+  const toggleRegisterModal = () => {
+    setIsRegisterOpen((prevState) => !prevState);
+  };
+
   return (
     <SC.AuthNavWrapper>
-      <SC.LoginBtn type="button">
+      <SC.LoginBtn type="button" onClick={toggleLoginModal}>
         <SC.LoginIcon />
         Log in
       </SC.LoginBtn>
-      <SC.RegisterBtn type="button">Registration</SC.RegisterBtn>
+      <SC.RegisterBtn type="button" onClick={toggleRegisterModal}>
+        Registration
+      </SC.RegisterBtn>
+      {isLoginOpen && <LoginModal onClose={toggleLoginModal} />}
+      {isRegisterOpen && <RegisterModal onClose={toggleRegisterModal} />}
     </SC.AuthNavWrapper>
   );
 };
