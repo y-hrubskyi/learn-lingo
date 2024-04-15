91680630407daa39ffce2b56fcbd06ebdfa9b520
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-15 21:35:54 +0300

    refactor: upd theming
     - upd themes objects
     - upd ThemeProvider structure
     - add theme state

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,8 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { Outlet, useLocation } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+import { getTheme } from "@/styles/theme";
 
 import { GlobalStyle } from "@/styles/GlobalStyle";
 import { Header } from "@/components/Header/Header";
@@ -7,18 +10,21 @@ import { Header } from "@/components/Header/Header";
 import * as SC from "./Layout.styled";
 
 export const Layout = () => {
+  const [theme, setTheme] = useState("yellow");
   const location = useLocation();
 
   return (
-    <SC.Container>
-      <GlobalStyle locationPath={location.pathname} />
+    <ThemeProvider theme={getTheme(theme)}>
+      <SC.Container>
+        <GlobalStyle locationPath={location.pathname} />
 
-      <Header />
-      <main>
-        <Suspense>
-          <Outlet />
-        </Suspense>
-      </main>
-    </SC.Container>
+        <Header />
+        <main>
+          <Suspense>
+            <Outlet />
+          </Suspense>
+        </main>
+      </SC.Container>
+    </ThemeProvider>
   );
 };
