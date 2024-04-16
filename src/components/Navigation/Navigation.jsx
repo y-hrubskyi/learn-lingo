5c69c7e147c84a1a47cd318893fbb10d37c9d9f0
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-16 21:47:59 +0300

    feat: upd Navigation
    Favorites link is available if user is logged in

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,10 @@
+import { useAuth } from "@/hooks/useAuth";
+
 import * as SC from "./Navigation.styled";
 
 export const Navigation = () => {
+  const { currentUser } = useAuth();
+
   return (
     <nav>
       <SC.NavList>
@@ -10,9 +14,11 @@ export const Navigation = () => {
         <li>
           <SC.NavLink to="/teachers">Teachers</SC.NavLink>
         </li>
-        <li>
-          <SC.NavLink to="/favorites">Favorites</SC.NavLink>
-        </li>
+        {currentUser && (
+          <li>
+            <SC.NavLink to="/favorites">Favorites</SC.NavLink>
+          </li>
+        )}
       </SC.NavList>
     </nav>
   );
