5bf1c0827887b049eae203964833c9fa66dd4590
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-16 22:05:51 +0300

    feat: add actual info about user

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,11 +1,15 @@
+import { useAuth } from "@/hooks/useAuth";
+
 import * as SC from "./UserMenu.styled";
 
 export const UserMenu = () => {
+  const { currentUser } = useAuth();
+
   return (
     <SC.UserMenuWrapper>
       <SC.User>
-        <SC.UserAvatar>U</SC.UserAvatar>
-        <SC.UserName>Username</SC.UserName>
+        <SC.UserAvatar>{currentUser.displayName[0]}</SC.UserAvatar>
+        <SC.UserName>{currentUser.displayName}</SC.UserName>
       </SC.User>
       <SC.LogoutBtn type="button">
         Log out
