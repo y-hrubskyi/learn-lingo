87cafc65fcc2f0773803e1c7afa2f7e698b129e6
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-16 21:21:13 +0300

    feat: set FavoritesPage as private route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 import { Layout } from "@/components/Layout/Layout";
+import { PrivateRoute } from "@/components/PrivateRoute/PrivateRoute";
 
 const HomePage = lazy(() => import("@/pages/HomePage/HomePage"));
 const TeachersPage = lazy(() => import("@/pages/TeachersPage/TeachersPage"));
@@ -13,7 +14,10 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="teachers" element={<TeachersPage />} />
-        <Route path="favorites" element={<FavoritesPage />} />
+        <Route
+          path="favorites"
+          element={<PrivateRoute component={<FavoritesPage />} />}
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
