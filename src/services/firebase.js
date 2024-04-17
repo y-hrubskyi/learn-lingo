f402c6f3e590ddc9ca9d0adbaaa99e7499f25ba6
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-17 22:16:06 +0300

    feat: config firebase remote database

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const {
   VITE_FIREBASE_API_KEY: API_KEY,
@@ -24,3 +25,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const db = getDatabase(app);
