9fb4456959a29fb14ecd2c2f55040accdd0b790f
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-17 22:22:03 +0300

    feat: fetch teachers using firebase realtime database
     - remova local techers data
     - implement fetching teachers data using firebase realtime database

--- src/components/TeacherList/TeacherList.jsx
+++ src/components/TeacherList/TeacherList.jsx
@@ -4,8 +4,8 @@ import * as SC from "./TeacherList.styled";
 export const TeacherList = ({ teachers }) => {
   return (
     <SC.TeacherList>
-      {teachers.map((teacher) => (
-        <TeacherItem key={teacher.id} teacher={teacher} />
+      {teachers.map(([key, teacher]) => (
+        <TeacherItem key={key} teacher={teacher} />
       ))}
     </SC.TeacherList>
   );
