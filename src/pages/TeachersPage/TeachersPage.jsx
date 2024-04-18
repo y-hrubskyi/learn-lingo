5218f9e83c5c042e8f38607b4080b83b12b26ec1
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-18 22:07:13 +0300

    feat: add pagination functionality for TeachersPage

--- src/pages/TeachersPage/TeachersPage.jsx
+++ src/pages/TeachersPage/TeachersPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { getTeachers } from "@/services/firebase";
 
@@ -7,17 +7,39 @@ import * as SC from "./TeachersPage.styled";
 
 const TeachersPage = () => {
   const [teachers, setTeachers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loadMore, setLoadMore] = useState(true);
+  const lastKey = useRef();
 
   useEffect(() => {
     (async () => {
-      const data = await getTeachers();
-      setTeachers(Object.entries(data));
+      try {
+        const data = await getTeachers(lastKey.current);
+
+        const keys = Object.keys(data);
+        const loadMore = keys.length >= 4;
+        lastKey.current = keys[keys.length - 1];
+
+        setTeachers((prevState) => [...prevState, ...Object.entries(data)]);
+        setLoadMore(loadMore);
+      } catch (error) {
+        console.log(error);
+      }
     })();
-  }, []);
+  }, [page]);
+
+  const handleLoadMore = () => {
+    setPage((prevState) => prevState + 1);
+  };
 
   return (
     <SC.PageWrapper>
       {teachers && <TeacherList teachers={teachers} />}
+      {loadMore && (
+        <SC.LoadMoreBtn type="button" onClick={handleLoadMore}>
+          Loading
+        </SC.LoadMoreBtn>
+      )}
     </SC.PageWrapper>
   );
 };
