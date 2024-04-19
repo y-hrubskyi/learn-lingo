b9029ffaedd46f445d4214a3efa18940c87df7f4
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-19 23:14:29 +0300

    feat: add favorites functionality to TeacherList
     - add checking "is teacher favorite?"
     - subscribe to changes in favorites

--- src/components/TeacherList/TeacherList.jsx
+++ src/components/TeacherList/TeacherList.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
+
+import { useAuth } from "@/hooks/useAuth";
+import { subscribeFavoriteKeys } from "@/services/firebase";
+
 import { TeacherItem } from "@/components/TeacherItem/TeacherItem";
 import * as SC from "./TeacherList.styled";
 
 export const TeacherList = ({ teachers }) => {
+  const [favoriteKeys, setFavoriteKeys] = useState(null);
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    if (!currentUser) return;
+
+    const unsubscribe = subscribeFavoriteKeys(currentUser.uid, setFavoriteKeys);
+
+    return unsubscribe;
+  }, [currentUser]);
+
   return (
     <SC.TeacherList>
       {teachers.map(([key, teacher]) => (
-        <TeacherItem key={key} teacher={teacher} />
+        <TeacherItem
+          key={key}
+          teacher={teacher}
+          isFavorite={currentUser && favoriteKeys?.includes(key)}
+        />
       ))}
     </SC.TeacherList>
   );
