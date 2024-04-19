0b9d4f2355c760b3926a5137831c146d28ce345c
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-19 23:28:06 +0300

    feat: implement FavoritesPage
     - get favoriteItems
     - subscribe to changes in favorites
     - reuse TeacherList and TeacherItem components with realised earlier functionality to render list of favorites

--- src/pages/FavoritesPage/FavoritesPage.jsx
+++ src/pages/FavoritesPage/FavoritesPage.jsx
@@ -1,5 +1,24 @@
+import { useEffect, useState } from "react";
+
+import { useAuth } from "@/hooks/useAuth";
+import { subscribeFavoriteItems } from "@/services/firebase";
+
+import { TeacherList } from "@/components/TeacherList/TeacherList";
+
 const FavoritesPage = () => {
-  return <div>FavoritesPage</div>;
+  const [favoriteItems, setFavoriteItems] = useState(null);
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    const unsubscribe = subscribeFavoriteItems(
+      currentUser.uid,
+      setFavoriteItems
+    );
+
+    return unsubscribe;
+  }, [currentUser]);
+
+  return favoriteItems && <TeacherList teachers={favoriteItems} />;
 };
 
 export default FavoritesPage;
