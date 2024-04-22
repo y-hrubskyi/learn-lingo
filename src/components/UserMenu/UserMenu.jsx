66446369bca6280735b41c688b92794bfc9dc66a
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-22 09:26:13 +0300

    feat: add toast-notifications for logout

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,25 +1,45 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 import { useAuth } from "@/hooks/useAuth";
 
+import { ToastMessage } from "@/components/common/ToastMessage/ToastMessage.styled";
 import * as SC from "./UserMenu.styled";
 
 export const UserMenu = () => {
-  const { currentUser, logOut } = useAuth();
+  const {
+    currentUser: { displayName },
+    logOut,
+  } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleLogout = async () => {
     try {
-      await logOut();
+      setIsLoading(true);
+
+      const logoutPromise = logOut();
+
+      await toast.promise(logoutPromise, {
+        loading: <ToastMessage>Logging out...</ToastMessage>,
+        success: <ToastMessage>Logout successful!</ToastMessage>,
+        error: <ToastMessage>Logout failed. Try again.</ToastMessage>,
+      });
     } catch (error) {
-      console.error(error);
+      // handled in toast.promise
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <SC.UserMenuWrapper>
-      <SC.User>
-        <SC.UserAvatar>{currentUser.displayName[0]}</SC.UserAvatar>
-        <SC.UserName>{currentUser.displayName}</SC.UserName>
-      </SC.User>
-      <SC.LogoutBtn type="button" onClick={handleLogout}>
+      {displayName && (
+        <SC.User>
+          <SC.UserAvatar>{displayName[0]}</SC.UserAvatar>
+          <SC.UserName>{displayName}</SC.UserName>
+        </SC.User>
+      )}
+      <SC.LogoutBtn type="button" onClick={handleLogout} disabled={isLoading}>
         Log out
         <SC.LogoutIcon />
       </SC.LogoutBtn>
