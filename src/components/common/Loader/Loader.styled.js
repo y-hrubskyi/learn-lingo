f73d1d69cbbb7c9a1589c45da820029d7e495c2f
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-22 09:42:27 +0300

    feat: create reusable Loader component for lists loading

---
+++ src/components/common/Loader/Loader.styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Loader = styled.div`
+  width: 48px;
+  height: 48px;
+  margin-top: ${(p) => p.theme.spacing(16)};
+  margin-left: auto;
+  margin-right: auto;
+
+  border-radius: 50%;
+  border-top: 3px solid ${(p) => p.theme.colors.accent};
+  border-right: 3px solid transparent;
+
+  animation: rotation 1s linear infinite;
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
