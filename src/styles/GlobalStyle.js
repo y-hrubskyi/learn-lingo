b71357bb540ef32af0238f0e475c9e9fed5670d0
y-hrubskyi <y.hrubskyi@gmail.com>
2024-04-25 23:14:44 +0300

    chore: upd formatting

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -9,44 +9,40 @@ import RobotoBoldWOFF2 from "@/assets/fonts/Roboto-Bold.woff2";
 export const GlobalStyle = createGlobalStyle`
   @font-face {
     font-family: "Roboto";
-    src: local("Roboto"),
-      url(${RobotoRegularWOFF2}) format("woff2");
-      font-weight: normal;
-      font-style: normal;
-    }
-    
-    @font-face {
-      font-family: "Roboto";
-    src: local("Roboto"),
-      url(${RobotoItalicWOFF2}) format("woff2");
-      font-weight: normal;
-      font-style: italic;
-    }
-    
-    @font-face {
-      font-family: "Roboto";
-    src: local("Roboto"),
-      url(${RobotoMediumWOFF2}) format("woff2");
-      font-weight: 500;
-      font-style: normal;
-    }
-    
-    @font-face {
-      font-family: "Roboto";
-      src: local("Roboto"),
-      url(${RobotoBoldWOFF2}) format("woff2");
-      font-weight: 700;
-      font-style: normal;
-    }
-    
-    body {
-      font-family: 'Roboto', sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      background-color: ${(p) =>
-        p.theme.colors[p.locationPath === "/" ? "primaryBg" : "secondaryBg"]};
+    src: local("Roboto"), url(${RobotoRegularWOFF2}) format("woff2");
+    font-weight: normal;
+    font-style: normal;
   }
-  
+
+  @font-face {
+    font-family: "Roboto";
+    src: local("Roboto"), url(${RobotoItalicWOFF2}) format("woff2");
+    font-weight: normal;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: "Roboto";
+    src: local("Roboto"), url(${RobotoMediumWOFF2}) format("woff2");
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Roboto";
+    src: local("Roboto"), url(${RobotoBoldWOFF2}) format("woff2");
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  body {
+    font-family: "Roboto", sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    background-color: ${(p) =>
+      p.theme.colors[p.locationPath === "/" ? "primaryBg" : "secondaryBg"]};
+  }
+
   h1,
   h2,
   h3,
@@ -56,20 +52,21 @@ export const GlobalStyle = createGlobalStyle`
   p {
     margin: 0;
   }
-  
+
   ul,
   ol {
     margin: 0;
     padding: 0;
     list-style: none;
   }
-  
+
   button {
     cursor: pointer;
   }
-  
+
   img {
     display: block;
     max-width: 100%;
     height: auto;
-  }`;
+  }
+`;
